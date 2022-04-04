9e7f966adc8bf93c88c4a57f209fd0c61c693659
AnnShi <shira99908@gmail.com>
2022-04-04 21:49:30 +0400

    first homework

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Anna on 03.04.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var myImage: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var textLabel: UITextView!
+    
+}
